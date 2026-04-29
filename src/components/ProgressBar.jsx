abba4bafdf77e52428aa2e707e71321bca66de98
Rush702 <rush702@gmail.com>
2026-04-29 12:01:21 -0700

    Remove logo image from ProgressBarRemove logo image from progress bar header
    Removed logo image from ProgressBar component.

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -17,7 +17,6 @@ export default function ProgressBar({ progress, section, sectionName, current, t
   return (
     <div className="progress-wrap">
       <div className="progress-brand">
-        <img src="logo.png" alt="SC Financial Life Group" className="progress-logo" />
         <span className="progress-brand-name">SC Financial Life Group</span>
       </div>
       <div className="progress-meta">
